7db3f6857745ddcec26aaa8d4f77faea71ccd6f6
Nondyebo Hlwempu <nondyeboh@gmail.com>
2019-03-18 12:41:51 +0200

    edited Author's details

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
-    author='<Your Name>',
-    author_email='<Your Email>'
+    url='https://github.com/Nondyebo/Mypackage/blob/master/setup.py',
+    author='Nondyebo',
+    author_email='nondyeboh@gmail.com'
 )
